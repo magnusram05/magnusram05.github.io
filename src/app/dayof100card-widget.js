f358296bc53864a9620955f31167f67a3373099b
magnusramgit <magnusramgit@gmail.com>
2018-11-21 16:47:41 +0530

    Material icon to indicate today

--- src/app/dayof100card-widget.js
+++ src/app/dayof100card-widget.js
@@ -1,17 +1,22 @@
 define(['knockout','jquery'], function(ko,$) {
     function DayOf100ViewModel() {
     	var obj = this;
-        this.data = ko.observableArray();	
+        this.round = ko.observable();	
+        this.days = ko.observableArray();        
         $.ajax({
 			  url: "src/app/data/data.json",
 			  dataType: "json",
 			  success: function(response) {
+			  	 obj.round(response.data.round);
 			  	 $.each(response.data.days, function(item,value) {
-			      obj.data.push(value);
+			      obj.days.push(value);
 			    });
 			  }
 			});	 
     } 
+    DayOf100ViewModel.prototype.id = function(index){
+    	return this.days().length-index;
+    }
     return DayOf100ViewModel;
 });
 
